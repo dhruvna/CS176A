416228d48c7776f99576e435d7354f00cb6e5864
Dhruv <dna@ucsb.edu>
2024-02-04 19:27:49 -0800

    Basic timeout functionality is in

--- HW3b/PingClient.c
+++ HW3b/PingClient.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
+#define TIMEOUT_SEC 1
 
 int main(int argc, char *argv[]) { //take in server ip and port
     if(argc != 3) {
@@ -19,6 +20,7 @@ int main(int argc, char *argv[]) { //take in server ip and port
     int sockfd;
     struct sockaddr_in serverAddr;
     char buffer[BUFFER_SIZE];
+    socklen_t serverAddrLen;
 
     // Create socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -35,24 +37,39 @@ int main(int argc, char *argv[]) { //take in server ip and port
         perror("Invalid address/ Address not supported");
         exit(EXIT_FAILURE);
     }
+
     // send 10 messages, one every second. Messages should be of the form "PING {#} {timestamp}"
     for (int i = 1; i <= 10; i++) {
+        struct timeval start_time, current_time;
+        gettimeofday(&start_time, NULL);
+
         sprintf(buffer, "PING %d %ld", i, time(NULL));
         if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
             perror("Failed to send data to server");
             exit(EXIT_FAILURE);
         }
-        //
-        sleep(1);
-    }
 
-    socklen_t serverAddrLen = sizeof(serverAddr);
-    while (1) {
-        // Receive data from server
+        // Set timeout for receive operation
+        struct timeval timeout;
+        timeout.tv_sec = TIMEOUT_SEC;
+        timeout.tv_usec = 0;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1) {
+            perror("setsockopt");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+        
+        // Receive response
         int len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&serverAddr, &serverAddrLen);
-        buffer[len] = '\0'; // Null-terminate the received string
-
-        printf("From server: %s\n", buffer);
+        if (len < 0) {
+            printf("Request timed out\n");
+        } else {
+            gettimeofday(&current_time, NULL);
+            double elapsed_time = (current_time.tv_sec - start_time.tv_sec) + (current_time.tv_usec - start_time.tv_usec) / 1000000.0;
+            printf("Received response in %.6lf seconds: %s\n", elapsed_time, buffer);
+        }
+        
+        sleep(1);
     }
 
     // Close socket
