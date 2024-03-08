f436ecb8dbf75ad401e671597db1f693a3be6c90
Dhruv Aggarwal <dna@ucsb.edu>
2024-03-08 09:25:34 -0800

    Simple TCP Stuff

---
+++ HW5/hangman_client.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 129 //128 is the max string length, include 1 more to null terminate it
+
+int main(int argc, char *argv[]) { //take in server ip and port
+    if(argc != 3) {
+        printf("Usage: %s <server_ip> <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    const char* SERVER_IP = argv[1];
+    int PORT = atoi(argv[2]);
+    int sockfd;
+    struct sockaddr_in serverAddr;
+    char buffer[BUFFER_SIZE];
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set server address
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &serverAddr.sin_addr) <= 0) { //bind to specific ip and port
+        perror("Invalid address/Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get input from user
+    printf("Enter string: "); 
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0; // Remove newline character or it will mess with server processing
+    // printf("Sending: %s\n", buffer);
+
+    // Send data to server
+    if (send(sockfd, buffer, strlen(buffer), 0) < 0) {
+        perror("Failed to send data to server");
+        exit(EXIT_FAILURE);
+    }
+    
+    int bytesReceived;
+    // Read data from the server
+    while ((bytesReceived = recv(sockfd, buffer, BUFFER_SIZE, 0)) > 0) {
+        buffer[bytesReceived] = '\0'; // Null-terminate the received data
+        
+        char *ptr = buffer; // Pointer to track the current position in the buffer
+        char *end; // Pointer to track the end of the current message
+        while ((end = strchr(ptr, '\n')) != NULL) {
+            *end = '\0'; // Replace the newline with a null terminator to create a string for the current message
+            if (*ptr) { // If the string is not empty
+                printf("From server: %s\n", ptr); // Print the message
+            }
+            ptr = end + 1; // Move past the current message
+        }
+
+        if (*ptr) { // If there's remaining text after the last newline, print it
+            printf("From server: %s\n", ptr);
+        }
+    }
+
+    if (bytesReceived < 0) {
+        perror("recv failed");
+    } else if (bytesReceived == 0) {
+        // printf("Server closed the connection.\n");
+    }
+    // Close the socket
+    close(sockfd);
+    return 0;
+}
---
+++ HW5/hangman_server.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 129 //128 is the max string length, include 1 more to null terminate it
+
+int main(int argc, char *argv[]) { // take in port number
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    int PORT = atoi(argv[1]);
+    int sockfd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    // Create socket 
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set socket options
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind socket to the specified port
+    if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Binding failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(sockfd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+        char buffer[BUFFER_SIZE];
+        char response[BUFFER_SIZE];
+        char tempBuffer[BUFFER_SIZE];
+
+        // Accept client connection
+        if ((new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        // Receive message from client
+        int len = recv(new_socket, buffer, BUFFER_SIZE, 0);
+        printf("Received: %s\n", buffer);
+
+        // Close the socket for this client
+        close(new_socket);
+    }
+    close(sockfd);
+    return 0;
+}
